63ccfa82bb58e223b6a0c0d12f035f189666f28a
Prince Mbeah Essilfie <pmessilfie@st.knust.edu.gh>
2023-05-26 17:09:50 -0700

    PercentageDimensionsBasic try

--- App.js
+++ App.js
@@ -34,6 +34,31 @@ const FixedDimensionBasics = () => {
       }} />
     </View>
   )
-}
+};
 
-export default FlexedDimensionBasics;
+const PercentageDimensionsBasic = () => {
+  return (
+    <View style={{ height: '100%' }}>
+      <View
+        style={{
+          height: '15%',
+          backgroundColor: 'powderblue'
+        }}
+      />
+      <View
+        style={{
+          width: '66%',
+          height: '35%',
+          backgroundColor: 'skyblue',
+        }}
+      />
+      <View style={{
+        width: '33%',
+        height: '50%',
+        backgroundColor: 'steelblue',
+      }}
+      />
+    </View>
+  );
+};
+export default PercentageDimensionsBasic;
